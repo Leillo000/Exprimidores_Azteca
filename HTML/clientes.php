f9c8da2bda32c09e40b641c3d4d0382edfece1cf
Leillo <o.leobernal@gmail.com>
2026-01-26 17:30:52 -0600

    Se agrego piezas, clientes, un svg de busqueda y estilos adicionales para acomodar la barra de busqueda de manera correcta, asi como un scroll para la tabla mediante un div

--- HTML/clientes.php
+++ HTML/clientes.php
@@ -1,6 +1,6 @@
 <?php
 include("../config/connection.php");
-
+include("../assets/HTML/layout.php");
 // Refactorizar esta parte en el futuro, mediante un array clave-valor
 /*
  return [ "datos" => $res->fetch_all(MYSQLI_ASSOC), "total" => $total, "totalPaginas" => $totalPaginas, "paginaActual" => $pagina ];
@@ -22,136 +22,99 @@ $total = intval($resultado_query["total"]);
 $totalPaginas = max(1, ceil($total / $PorPagina)); // calcula en cuántas páginas mostrar los registros
 
 $stmt = $conexion->prepare(
-"SELECT 
+    "SELECT 
 e.nombre, e.rfc, e.correo, e.telefono, c.fecha_registro, c.id_cliente
 FROM clientes AS c
 JOIN empresas AS e ON e.id_cliente = c.id_cliente
 WHERE activo = 1
-ORDER BY nombre DESC LIMIT ? OFFSET ?");
+ORDER BY nombre DESC LIMIT ? OFFSET ?"
+);
 $stmt->bind_param("ii", $PorPagina, $offset);
 $stmt->execute();
 $res = $stmt->get_result();
 
 ?>
 
-<!DOCTYPE html>
-<html lang="es">
-
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="http://localhost/Codigo_Exprimidores_Azteca/CSS/pedidos.css">
-    <link rel="stylesheet" href="http://localhost/Codigo_Exprimidores_Azteca/assets/CSS/nav.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Alan+Sans:wght@300..900&family=Annapurna+SIL:wght@400;700&family=Arimo:ital,wght@0,400..700;1,400..700&family=Bricolage+Grotesque:opsz,wght@12..96,200..800&family=Epunda+Sans:ital,wght@0,300..900;1,300..900&family=Epunda+Slab:ital,wght@0,300..900;1,300..900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
-        rel="stylesheet">
-    <link href="../Images/logo_icon.ico" rel="icon" type="image/x-icon">
     <title> Clientes </title>
 </head>
 
 <body>
-    <nav>
-        <img src="../Images/logo_menu.jpg" alt="logo_exprimidores_azteca">
-        <ul>
-            <div id="separate_link">
-                <li>
-                    <a href="piezas.php"> Piezas </a>
-                </li>
-                <li>
-                    <a href="pedidos.php"> Pedidos </a>
-                </li>
-                <li>
-                    <a href="productos.php"> Productos </a>
-                </li>
-                <li>
-                    <a href="materiales.php"> Materiales </a>
-                </li>
-                <li>
-                    <a href="carrito.php">
-                        <!-- Icono de carrito -->
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                            stroke="#2F6842" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                            class="icon icon-tabler icons-tabler-outline icon-tabler-shopping-cart">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                            <path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
-                            <path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
-                            <path d="M17 17h-11v-14h-2" />
-                            <path d="M6 5l14 1l-1 7h-13" />
-                    </a>
-                </li>
-            </div>
-        </ul>
-    </nav>
-    <br>
-    <div>
+    <div class="container">
+
         <h1> Clientes </h1>
         <br>
-    </div>
-    <div>
-        <table>
-            <thead>
-                <tr>
-                    <th class="columnas"> Nombre </th>
-                    <th class="columnas"> Correo </th>
-                    <th class="columnas"> Teléfono </th>
-                    <th class="columnas"> Fecha registro </th>
-                    <th class="columnas"> Fecha </th>
-                    <th class="columnas"> Acción </th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php while ($row = $res->fetch_assoc()): ?>
+        <div class="table_scroll">
+            <table>
+                <thead>
                     <tr>
-                        <td> <?php echo htmlspecialchars($row['nombre']); ?> </td>
-                        <td> <?php echo htmlspecialchars($row['rfc']); ?> </td>
-                        <td> <?php echo htmlspecialchars($row['correo']); ?> </td>
-                        <td> <?php echo htmlspecialchars($row['telefono']); ?> </td>
-                        <td> <?php echo htmlspecialchars($row['fecha_registro']); ?> </td>
-                        <td>
-                            <!-- Después de cada onchange, se invoca la función de Javascript junto a lo que se quiera hacer -->
-                            <select class="button_table" onchange="redirigir(this.value, <?php echo $row["id_cliente"]; ?>)">
-                                <option class="button_table" value="" disabled selected hidden> Seleccionar acción </option>
-                                <option class="button_table" value="eliminar"> Eliminar </option>
-                                <option class="button_table" value="editar"> Editar </option>
-                            </select>
-                        </td>
+                        <th class="columnas"> Nombre </th>
+                        <th class="columnas"> Correo </th>
+                        <th class="columnas"> Teléfono </th>
+                        <th class="columnas"> Fecha registro </th>
+                        <th class="columnas"> Fecha </th>
+                        <th class="columnas"> Acción </th>
                     </tr>
-                <?php endwhile; ?>
-            </tbody>
-        </table>
-    </div>
-    <div id="control_pages">
-        <?php if ($Pagina > 1) { ?>
-            <a href="?page=1">
-                <b>
-                    << Primero</b></a>
-            <a href="?page=<?php echo $Pagina - 1 ?>"><b>
-                    < Anterior</b></a>
-        <?php } else if ($Pagina = 1) { ?>
-                <!-- En caso de estar en la primera página se "desactivan" los links para ir a la siguiente pagina -->
-                <p>
-                    << Primero</p>
-                        <p>
-                            < Anterior</p>
-                        <?php } ?>
-                        <p> Página <?php echo $Pagina; ?> de <?php echo $totalPaginas; ?></p>
-                        <?php
-                        // En caso de que la pagina actual sea la ultima, los links se "desactivan", pero solo se pone un parrafo a su vez
-                        if ($Pagina == $totalPaginas) { ?>
-                            <p>Siguiente</p>
-                            <p>Última página >></p>
-                        <?php } else if ($Pagina < $totalPaginas) {
-                            ?>
-                                <a href="?page=<?php echo $Pagina + 1; ?>"><b> Siguiente</b></a>
-                                <a href="?page=<?php echo $totalPaginas; ?>"><b>Última página >></b> </a>
-                        <?php } ?>
+                </thead>
+                <tbody>
+                    <?php while ($row = $res->fetch_assoc()): ?>
+                        <tr>
+                            <td>
+                                <?php echo htmlspecialchars($row['nombre']); ?>
+                            </td>
+                            <td>
+                                <?php echo htmlspecialchars($row['rfc']); ?>
+                            </td>
+                            <td>
+                                <?php echo htmlspecialchars($row['correo']); ?>
+                            </td>
+                            <td>
+                                <?php echo htmlspecialchars($row['telefono']); ?>
+                            </td>
+                            <td>
+                                <?php echo htmlspecialchars($row['fecha_registro']); ?>
+                            </td>
+                            <td>
+                                <!-- Después de cada onchange, se invoca la función de Javascript junto a lo que se quiera hacer -->
+                                <select class="button_table" onchange="redirigir(this.value, <?php echo $row["id_cliente"]; ?>)">
+                                    <option class="button_table" value="" disabled selected hidden> Seleccionar acción
+                                    </option>
+                                    <option class="button_table" value="eliminar"> Eliminar </option>
+                                    <option class="button_table" value="editar"> Editar </option>
+                                </select>
+                            </td>
+                        </tr>
+                    <?php endwhile; ?>
+                </tbody>
+            </table>
+        </div>
 
+        <div class="center_items">
+            <?php if ($Pagina > 1) { ?>
+                <a href="?page=1">
+                    <b>
+                        << Primero</b></a>
+                <a href="?page=<?php echo $Pagina - 1 ?>"><b>
+                        < Anterior</b></a>
+            <?php } else if ($Pagina = 1) { ?>
+                    <!-- En caso de estar en la primera página se "desactivan" los links para ir a la siguiente pagina -->
+                    <p>
+                        << Primero</p>
+                            <p>
+                                < Anterior</p>
+                            <?php } ?>
+                            <p> Página <?php echo $Pagina; ?> de <?php echo $totalPaginas; ?></p>
+                            <?php
+                            // En caso de que la pagina actual sea la ultima, los links se "desactivan", pero solo se pone un parrafo a su vez
+                            if ($Pagina == $totalPaginas) { ?>
+                                <p>Siguiente</p>
+                                <p>Última página >></p>
+                            <?php } else if ($Pagina < $totalPaginas) {
+                                ?>
+                                    <a href="?page=<?php echo $Pagina + 1; ?>"><b> Siguiente</b></a>
+                                    <a href="?page=<?php echo $totalPaginas; ?>"><b>Última página >></b> </a>
+                            <?php } ?>
+
+        </div>
     </div>
-    <button class="button" onclick="location.href='agregar_clientes.php'"> Agregar </button>
-    <button class="button" onclick="location.href='menu.php'"> MENÚ </button>
     <script src="../assets/JS/clientes.js"></script>
-</body>
-
-</html>
--- HTML/piezas.php
+++ HTML/piezas.php
@@ -1,6 +1,7 @@
 <?php
 include("../config/connection.php");
 include("../helpers/utils.php");
+include("../assets/HTML/layout.php");
 
 $query = "SELECT ps.id_pieza, po.nombre_producto, ps.nombre_pieza, ps.peso FROM piezas AS ps 
 JOIN productos AS po ON po.id_producto = ps.id_producto 
@@ -21,69 +22,30 @@ $stmt->execute();
 $res = $stmt->get_result();
 ?>
 
-<!DOCTYPE html>
-<html lang="es">
-
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="http://localhost/Codigo_Exprimidores_Azteca/CSS/tabla_accion.css">
-    <link rel="stylesheet" href="http://localhost/Codigo_Exprimidores_Azteca/assets/CSS/nav.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Alan+Sans:wght@300..900&family=Annapurna+SIL:wght@400;700&family=Arimo:ital,wght@0,400..700;1,400..700&family=Bricolage+Grotesque:opsz,wght@12..96,200..800&family=Epunda+Sans:ital,wght@0,300..900;1,300..900&family=Epunda+Slab:ital,wght@0,300..900;1,300..900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
-        rel="stylesheet">
-    <link href="../Images/logo_icon.ico" rel="icon" type="image/x-icon">
     <title> Pedidos </title>
 </head>
 
 <body>
-    <nav>
-        <img src="../Images/logo_menu.jpg" alt="logo_exprimidores_azteca">
-        <ul>
-            <div id="separate_link">
-                <li>
-                    <a href="piezas.php"> Piezas </a>
-                </li>   
-                <li>
-                    <a href="productos.php"> Productos </a>
-                </li>
-                <li>
-                    <a href="pedidos.php"> Pedidos </a>
-                </li> 
-                <li>
-                    <a href="materiales.php"> Materiales </a>
-                </li>
-                <li>
-                    <a href="carrito.php">
-                        <!-- Icono de carrito -->
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                            stroke="#2F6842" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                            class="icon icon-tabler icons-tabler-outline icon-tabler-shopping-cart">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                            <path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
-                            <path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
-                            <path d="M17 17h-11v-14h-2" />
-                            <path d="M6 5l14 1l-1 7h-13" />
-                    </a>
-                </li>
-            </div>
-        </ul>
-    </nav>
-    <br>
-    <div>
+    <div class="container">
         <h1> Piezas </h1>
         <br>
         <form method="post" action="piezas.php">
-            <span> <input name="nombre_pieza" type="text" placeholder="Buscar pieza por producto... ">
-                <button class="button_search" href="" alt="buscar" type="submit">
-                    <img src="../Images/lupa_buscar.png" alt="icono_buscar" class="icon_search">
-                </button> </span>
+            <!-- Este div lo que hace es poner en una sola línea (y centrados) el boton para buscar y el input que es la barra de busqueda -->
+            <div class="search_container">
+                <input name="nombre_pieza" type="text" placeholder="Buscar pieza por producto... ">
+                <button class="button_search" type="submit">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="30" viewBox="0 0 24 24" fill="none"
+                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                        class="icon icon-tabler icons-tabler-outline icon-tabler-search">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M3 10a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+                        <path d="M21 21l-6 -6" />
+                    </svg>
+                </button>
+            </div>
         </form>
         <br>
-    </div>
-    <div>
         <table>
             <thead>
                 <tr>
@@ -120,9 +82,4 @@ $res = $stmt->get_result();
             </tbody>
         </table>
     </div>
-    <!-- INSERTAR PIEZA -->
-    <button class="button" onclick="location.href='insertar_piezas.php'">Agregar una nueva pieza</button>
-    <button class="button" onclick="location.href='menu.php'"> MENÚ </button>
 </body>
-
-</html>
