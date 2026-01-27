300281d39a9e6972416bc5175813765ee51c34fe
Leillo <o.leobernal@gmail.com>
2026-01-26 18:22:25 -0600

    Se agrego la slidebar a agregar_clientes.php, carrito.php y materiales.php. Existen pequeños bugs con el cuadro de dialogo, y aun no se crea el apartado de agregar productos y falta implementar la slidebar a tomar_pedido.php

--- HTML/materiales.php
+++ HTML/materiales.php
@@ -1,5 +1,6 @@
 <?php
 include("../config/connection.php");
+include("../assets/HTML/layout.php");
 
 $PorPagina = 25;
 $Pagina = isset($_GET['page']) && is_numeric($_GET['page']) ? intval($_GET['page']) : 1;
@@ -24,102 +25,56 @@ $res = $stmt->get_result();
 
 ?>
 
-<!DOCTYPE html>
-<html lang="es">
-
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="http://localhost/Codigo_Exprimidores_Azteca/CSS/materiales.css">
-    <link rel="stylesheet" href="http://localhost/Codigo_Exprimidores_Azteca/assets/CSS/nav.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Alan+Sans:wght@300..900&family=Annapurna+SIL:wght@400;700&family=Arimo:ital,wght@0,400..700;1,400..700&family=Bricolage+Grotesque:opsz,wght@12..96,200..800&family=Epunda+Sans:ital,wght@0,300..900;1,300..900&family=Epunda+Slab:ital,wght@0,300..900;1,300..900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
-        rel="stylesheet">
-    <link href="../Images/logo_icon.ico" rel="icon" type="image/x-icon">
     <title> Materiales </title>
+</head>
 
 <body>
-    <nav>
-        <img src="../Images/logo_menu.jpg" alt="logo_exprimidores_azteca">
-        <ul>
-            <div id="separate_link">
-                <li>
-                    <a href="piezas.php"> Piezas </a>
-                </li>
-                <li>
-                    <a href="pedidos.php"> Pedidos </a>
-                </li>
-                <li>
-                    <a href="productos.php"> Productos </a>
-                </li>
-                <li>
-                    <a href="clientes">Clientes</a>
-                </li>   
-                <li>
-                    <a href="carrito.php">
-                        <!-- Icono de carrito -->
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                            stroke="#2F6842" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                            class="icon icon-tabler icons-tabler-outline icon-tabler-shopping-cart">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                            <path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
-                            <path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
-                            <path d="M17 17h-11v-14h-2" />
-                            <path d="M6 5l14 1l-1 7h-13" />
-                    </a>
-                </li>
-            </div>
-        </ul>
-    </nav>
-    <br>
-    <div class="center">
+
+    <div class="container">
         <h1> Materiales </h1>
-    </div>
-    <table>
-        <tr>
-            <th>No. de Registro</th>
-            <th>Cantidad total de aluminio en Kg</th>
-            <th>Fecha y hora de registro</th>
-        </tr>
+        <br>
+        <div class="center_items">
+            <table>
+                <tr>
+                    <th class="columnas">No. de Registro</th>
+                    <th class="columnas">Cantidad total de aluminio en Kg</th>
+                    <th class="columnas">Fecha y hora de registro</th>
+                </tr>
 
-        <?php while ($row = $res->fetch_assoc()) { ?>
-            <tr>
-                <td> <?php echo $row["no_registro"]; ?></td>
-                <td><?php echo $row["cantidad"]; ?></td>
-                <td><?php echo $row["fecha"]; ?></td>
-            </tr>
-        <?php } ?>
-    </table>
-    <div id="control_pages">
-        <?php if ($Pagina > 1) { ?>
-            <a href="?page=1">
-                <b><< Primero</b></a>
-                    <a href="?page=<?php echo $Pagina - 1 ?>"><b>< Anterior</b></a>
-                <?php } 
-                else if ($Pagina = 1){?>
-                <!-- En caso de estar en la primera página se "desactivan" los links para ir a la siguiente pagina -->
-                <p><< Primero</p>
-                <p>< Anterior</p>
+                <?php while ($row = $res->fetch_assoc()) { ?>
+                    <tr>
+                        <td> <?php echo $row["no_registro"]; ?></td>
+                        <td><?php echo $row["cantidad"]; ?></td>
+                        <td><?php echo $row["fecha"]; ?></td>
+                    </tr>
                 <?php } ?>
-                <p> Página <?php echo $Pagina; ?> de <?php echo $totalPaginas; ?></p>
-                <?php
-                // En caso de que la pagina actual sea la ultima, los links se "desactivan", pero solo se pone un parrafo a su vez
-                if($Pagina == $totalPaginas){?>
-                    <p>Siguiente</p>
-                    <p>Última página >></p>
-               <?php }
-                 else if ($Pagina < $totalPaginas) {
-                    ?>
-                    <a href="?page=<?php echo $Pagina + 1; ?>"><b> Siguiente</b></a>
-                    <a href="?page=<?php echo $totalPaginas; ?>"><b>Última página >></b> </a>
-                <?php } ?>
+            </table>
+            <?php if ($Pagina > 1) { ?>
+                <a href="?page=1">
+                    <b>
+                        << Primero</b></a>
+                <a href="?page=<?php echo $Pagina - 1 ?>"><b>
+                        < Anterior</b></a>
+            <?php } else if ($Pagina = 1) { ?>
+                    <!-- En caso de estar en la primera página se "desactivan" los links para ir a la siguiente pagina -->
+                    <p>
+                        << Primero</p>
+                            <p>
+                                < Anterior</p>
+                            <?php } ?>
+                            <p> Página <?php echo $Pagina; ?> de <?php echo $totalPaginas; ?></p>
+                            <?php
+                            // En caso de que la pagina actual sea la ultima, los links se "desactivan", pero solo se pone un parrafo a su vez
+                            if ($Pagina == $totalPaginas) { ?>
+                                <p>Siguiente</p>
+                                <p>Última página >></p>
+                            <?php } else if ($Pagina < $totalPaginas) {
+                                ?>
+                                    <a href="?page=<?php echo $Pagina + 1; ?>"><b> Siguiente</b></a>
+                                    <a href="?page=<?php echo $totalPaginas; ?>"><b>Última página >></b> </a>
+                            <?php } ?>
 
+        </div>
     </div>
-    
-            <button class="button" onclick="location.href='agregar_aluminio.php'"> Agregar stock </button>
-            <button class="button" onclick="location.href='menu.php'"> MENÚ </button>
 </body>
-
-</html>
