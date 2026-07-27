8d64c40557f453aeb72689accabf31aba82ff707
Leillo <o.leobernal@gmail.com>
2026-07-26 21:23:49 -0600

    Agregué un nuevo Singleton para crear una conexión, con el fin de crear una nueva clase que me sirvapara hacer consultas sin hacer tantas líneas de código

---
+++ helpers/connection.php
@@ -0,0 +1,26 @@
+<?php
+class Database {
+    private static $instance = null;
+    private $pdo;
+
+    private function __construct() {
+        $this->pdo = new PDO("mysql:host=localhost;dbname=exprimidores_azteca", "root", "");
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    public static function getInstance() {
+        if (self::$instance === null) {
+            self::$instance = new Database();
+        }
+        return self::$instance;
+    }
+
+    public function doQuery($sql, $params = []) {
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute($params);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
+
+
+?>
