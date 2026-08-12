3df5e4ad8da136495e8df4da92caf682f174a442
Leillo <o.leobernal@gmail.com>
2026-08-11 21:25:06 -0600

    Pequeños cambios

--- controllers/PHP/piezas.php
+++ controllers/PHP/piezas.php
@@ -6,9 +6,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET' && !empty($_GET)) {
     $id_pieza = isset($_GET['id_pieza']) ? intval($_GET['id_pieza']) : 0;
 
     $stmt = $conexion->prepare('SELECT ps.id_pieza, po.nombre_producto, ps.nombre_pieza, ps.peso, po.id_producto 
-FROM piezas AS ps 
-JOIN productos AS po ON po.id_producto = ps.id_producto 
-WHERE ps.id_pieza= ?');
+    FROM piezas AS ps 
+    JOIN productos AS po ON po.id_producto = ps.id_producto 
+    WHERE ps.id_pieza= ?');
 
     $stmt->bind_param('i', $id_pieza);
 
@@ -17,26 +17,27 @@ WHERE ps.id_pieza= ?');
         $Response = $StmtResut->fetch_assoc();
         echo json_encode($Response);
 
-    }  else {
+    } else {
         http_response_code(500);
         echo json_encode(['Status' => 'Error', 'Message' => 'Algo salió mal.']);
-    } 
+    }
 } else if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST)) {
-        $db = Database::getDatabase();
-        $id_pieza = isset($_POST['id_pieza']) ? intval($_POST['id_pieza']) : 0;
-        $id_producto = isset($_POST['id_producto']) ? intval($_POST['id_producto']) : 0;
-        $peso = isset($_POST['peso']) ? intval($_POST['peso']) : 0;
+    $db = Database::getDatabase();
+    $id_pieza = isset($_POST['id_pieza']) ? intval($_POST['id_pieza']) : 0;
+    $id_producto = isset($_POST['id_producto']) ? intval($_POST['id_producto']) : 0;
+    $peso = isset($_POST['peso']) ? intval($_POST['peso']) : 0;
+    $nombre_pieza = isset($_POST['nombre_pieza']) ? trim($_POST['nombre_pieza']) : "";
 
-        if ($id_pieza <= 0 || $id_producto <= 0 || $peso <= 0) {
-            echo json_encode(["status" => "error", "message" => "Valores inválidos"]);
-            exit();
-        }
-
-        $db->doQuery("UPDATE piezas SET peso = ? WHERE id_pieza = ?", [$peso, $id_pieza]);
-        $pesoTotalProducto = $db->doQuery("SELECT SUM(peso) AS peso_total FROM piezas WHERE id_producto = ?", [$id_producto]);
-        $db->doQuery("UPDATE productos SET peso = ? WHERE id_producto = ? AND activo = 1", [$pesoTotalProducto[0]["peso_total"], $id_producto]);
-        echo json_encode(["status" => "success"]);
+    if ($id_pieza <= 0 || $id_producto <= 0 || $peso <= 0 || empty($nombre_pieza)) {
+        echo json_encode(["status" => "error", "message" => "Valores inválidos"]);
         exit();
     }
+
+    $db->doQuery("UPDATE piezas SET peso = ?, nombre_pieza = ? WHERE id_pieza = ?", [$peso, $nombre_pieza, $id_pieza]);
+    $pesoTotalProducto = $db->doQuery("SELECT SUM(peso) AS peso_total FROM piezas WHERE id_producto = ?", [$id_producto]);
+    $db->doQuery("UPDATE productos SET peso = ? WHERE id_producto = ? AND activo = 1", [$pesoTotalProducto[0]["peso_total"], $id_producto]);
+    echo json_encode(["status" => "success"]);
+    exit();
+}
 
 ?>
